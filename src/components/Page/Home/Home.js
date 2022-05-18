dd63638705612261d8a19daab9030d9360b273f9
AimanKM <aiman@jisr.net>
2022-05-18 17:27:26 +0300

    auth express

--- src/components/Page/Home/Home.js
+++ src/components/Page/Home/Home.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import { ApiUtils } from 'utils/apiUtils';
+// import auth from 'utils/firebase';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import LestCard from 'components/organisms/LestCard';
+// import axios from 'axios';
 
 const Home = () => {
   const [page, setPage] = useState(1);
+  // const [accessToken, setAccessToken] = useState('');
 
   function fetchData() {
     return ApiUtils('character', { page: page });
   }
 
   const { data, status } = useQuery(['characters', page], fetchData);
+  // auth.currentUser
+  //   .getIdToken()
+  //   .then((accessToken) => setAccessToken(accessToken));
+
+  // React.useEffect(() => {
+  //   axios
+  //     .get('http://localhost:5000/api/users', {
+  //       headers: {
+  //         Accept: 'application/json',
+  //         'Content-Type': 'application/json',
+  //         'Access-Token': accessToken,
+  //       },
+  //     })
+  //     .then((response) => {
+  //       console.log('response', response);
+  //     });
+  // }, [accessToken]);
   return status === 'loading' ? (
     <div>
       <h3>loading......</h3>
