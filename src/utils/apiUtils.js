011723d26251c31cb5123e34d6a0c71d102b478f
AimanKM <aiman@jisr.net>
2022-05-19 12:19:13 +0300

    f2

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -16,4 +16,26 @@ function ApiUtils(url, params, method = 'GET', data = null) {
   });
 }
 
-export { ApiUtils };
+function ApiUtilsExpress({
+  url = '',
+  params = {},
+  method = 'GET',
+  data = null,
+}) {
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'Access-Token': sessionStorage.getItem('accessToken'),
+  };
+
+  return axios({
+    baseURL: 'http://localhost:5000/api/',
+    method,
+    url,
+    params,
+    data,
+    headers,
+  });
+}
+
+export { ApiUtils, ApiUtilsExpress };
